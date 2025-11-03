0acbe524ce9dd51a8de4df25a9d99a115f3059fb
Wen Bo Li <wen@wentron4.local>
2025-11-02 21:28:38 -0800

    add sample rate arg during preprocessing and change max_memory to just one element to see if that avoids trying to get a second GPU

--- src/model/pt_llm.py
+++ src/model/pt_llm.py
@@ -31,7 +31,8 @@ class PromptTuningLLM(torch.nn.Module):
 
         print('Loading LLAMA')
         kwargs = {
-            "max_memory": {0: '80GiB', 1: '80GiB'},
+            #"max_memory": {0: '80GiB', 1: '80GiB'},
+            "max_memory": {0: '80GiB'},
             "device_map": "auto",
             "revision": "main",
         }
